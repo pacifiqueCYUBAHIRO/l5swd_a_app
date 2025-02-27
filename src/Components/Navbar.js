d4b7913410b0583f8d298347e3707f759f7b4531
pacifiqueCYUBAHIRO <118057508+pacifiqueCYUBAHIRO@users.noreply.github.com>
2025-02-27 12:04:23 +0200

    Initial2 commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         <p className="text-xl p-1 font-bold">Logo</p>
         <ul className="flex space-x-6">
           <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
-          <li><Link to ="/" className="hover:text-gray-300">About</Link></li>
+          <li><Link to ="/" className="hover:text-gray-310">About</Link></li>
           <li><Link to ="/" className="hover:text-gray-300">Services</Link></li>
           <li><Link to ="/" className="hover:text-gray-300">Contact</Link></li>
         </ul>
